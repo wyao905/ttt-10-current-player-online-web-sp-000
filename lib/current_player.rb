3b4d5a90ef9060114f64fa59a868ff6ee35e2d16
Warren Yao <warrenyca905@gmail.com>
2019-05-11 17:28:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   count = 0
   index = 0
   board.each do |turn|
-    if board[index] != "X" || board[index] != "O"
+    if board[index] == "X" || board[index] == "O"
       return count += 1
     end
     index += 1
